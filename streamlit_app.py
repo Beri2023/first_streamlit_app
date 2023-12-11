7af3c1b56c541c1d6fd15da6227eaf3ac2061a3c
Beri2023 <128941059+Beri2023@users.noreply.github.com>
2023-12-10 21:52:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 #streamlit.text(fruityvice_response.json())
@@ -42,4 +41,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#allow the end user to add a fruit to the list
+add_my_furuit= streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding', add_my_furuit)
 
